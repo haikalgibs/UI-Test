0f54bfa08612d9090e1909342fe2d5af2d5393ad
haikalgibs <42896957+haikalgibs@users.noreply.github.com>
2020-12-12 01:17:18 +0700

    Membuat widget dengan tampilan statis

---
+++ app/src/main/java/org/d3if1059/mobpro2/helloworld/widget/CovidWidgetProvider.kt
@@ -0,0 +1,13 @@
+package org.d3if1059.mobpro2.helloworld.widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+class CovidWidgetProvider : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context?, manager: AppWidgetManager?,
+        ids: IntArray?
+    ) {
+    }
+}
